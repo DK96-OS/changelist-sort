56960f4ddaea2fddffe3d2ba8c1e992e08bbf0d2
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2024-09-05 08:48:48 -0400

    Input Package Tests: * Update test_argument_parser * Update test_init

--- test/input/test_init.py
+++ test/input/test_init.py
@@ -4,6 +4,7 @@ import pytest
 from pathlib import Path
 
 from changelist_sort.input import validate_input
+from changelist_sort.sorting.sort_mode import SortMode
 from test import data_provider
 
 
@@ -82,4 +83,25 @@ def test_validate_input_ws_path_arg_does_not_exist_raises_exit():
             assert False
         except SystemExit:
             assert True
+
+
+def test_validate_input_developer_sort():
+    test_input = ['-d']
+    with pytest.MonkeyPatch().context() as c:
+        c.setattr(Path, 'exists', lambda _: True)
+        c.setattr(Path, 'read_text', lambda _: data_provider.get_no_changelist_xml())
+        result = validate_input(test_input)
+        assert result.workspace_path == Path('.idea/workspace.xml')
+        assert result.sort_mode == SortMode.DEVELOPER
+
+
+def test_validate_input_sourceset_sort():
+    test_input = ['-s']
+    with pytest.MonkeyPatch().context() as c:
+        c.setattr(Path, 'exists', lambda _: True)
+        c.setattr(Path, 'read_text', lambda _: data_provider.get_no_changelist_xml())
+        result = validate_input(test_input)
+        assert result.workspace_path == Path('.idea/workspace.xml')
+        assert result.sort_mode == SortMode.SOURCESET
+
 
