3a8344dddacee5958d60edf91bb09603715e96e7
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2024-09-06 11:42:39 -0400

    Feature: Remove Empty Changelists Option (#7)
    Documentation:
    * Update README.md - add section on the remove flag
    
    Input Package Tests:
    * Update test_argument_parser - add test cases
    * Update test_init - add test cases
    
    Input Package:
    * Update __init__ - implement remove_empty
    * Update argument_data - add remove_empty field
    * Update argument_parser - implement remove_empty
    * Update input_data - add remove_empty field
    * Update string_validation - remove newline
    
    Main Source Package:
    * Update __init__ - implement remove_empty
    
    Project Root:
    * Update setup.py - increase version to 0.2.1
    
    Tests:
    * Update test_init - add test cases

--- changelist_sort/__init__.py
+++ changelist_sort/__init__.py
@@ -16,6 +16,10 @@ def sort_changelists(
         ws_tree.extract_list_elements(),
         input_data.sort_mode
     )
+    if input_data.remove_empty:
+        sorted_lists = list(filter(
+            lambda x: len(x.changes) > 0, sorted_lists
+        ))
     ws_tree.replace_changelists(sorted_lists)
     workspace.write_workspace_tree(
         ws_tree, input_data.workspace_path
--- test/test_init.py
+++ test/test_init.py
@@ -5,6 +5,7 @@ from xml.etree.ElementTree import ElementTree
 
 import pytest
 
+from changelist_sort.workspace.workspace_tree import WorkspaceTree
 from test import data_provider
 from changelist_sort import sort_changelists
 from changelist_sort.input.input_data import InputData
@@ -17,9 +18,11 @@ def save_write(
 ):
     global TAG
     global VERSION
+    global WS_TREE
     elem = self.getroot()
     TAG = elem.tag
     VERSION = elem.attrib['version']
+    WS_TREE = WorkspaceTree(elem)
 
 
 def test_sort_changelists_simple_module_sort_returns_xml():
@@ -33,3 +36,84 @@ def test_sort_changelists_simple_module_sort_returns_xml():
         sort_changelists(test_input)
     assert TAG == 'project'
     assert VERSION == '4'
+    result = WS_TREE.extract_list_elements()
+    assert len(result) == 2
+
+
+def test_sort_changelists_simple_module_sort_remove_empty_returns_xml():
+    test_input = InputData(
+        workspace_path=Path('file'),
+        workspace_xml=data_provider.get_simple_changelist_xml(),
+        sort_mode=SortMode.MODULE,
+        remove_empty=True,
+    )
+    with (pytest.MonkeyPatch().context() as c):
+        c.setattr(ElementTree, 'write', save_write)
+        sort_changelists(test_input)
+    assert TAG == 'project'
+    assert VERSION == '4'
+    result = WS_TREE.extract_list_elements()
+    assert len(result) == 1
+
+
+def test_sort_changelists_multi_cl_returns_xml():
+    test_input = InputData(
+        workspace_path=Path('file'),
+        workspace_xml=data_provider.get_multi_changelist_xml(),
+        sort_mode=SortMode.MODULE,
+    )
+    with (pytest.MonkeyPatch().context() as c):
+        c.setattr(ElementTree, 'write', save_write)
+        sort_changelists(test_input)
+    assert TAG == 'project'
+    assert VERSION == '4'
+    result = WS_TREE.extract_list_elements()
+    assert len(result) == 3
+
+
+def test_sort_changelists_multi_cl_remove_empty_returns_xml():
+    test_input = InputData(
+        workspace_path=Path('file'),
+        workspace_xml=data_provider.get_multi_changelist_xml(),
+        sort_mode=SortMode.MODULE,
+        remove_empty=True,
+    )
+    with (pytest.MonkeyPatch().context() as c):
+        c.setattr(ElementTree, 'write', save_write)
+        sort_changelists(test_input)
+    assert TAG == 'project'
+    assert VERSION == '4'
+    result = WS_TREE.extract_list_elements()
+    assert len(result) == 2
+
+
+def test_sort_changelists_multi_cl_sourceset_sort_returns_xml():
+    test_input = InputData(
+        workspace_path=Path('file'),
+        workspace_xml=data_provider.get_multi_changelist_xml(),
+        sort_mode=SortMode.SOURCESET,
+    )
+    with (pytest.MonkeyPatch().context() as c):
+        c.setattr(ElementTree, 'write', save_write)
+        sort_changelists(test_input)
+    assert TAG == 'project'
+    assert VERSION == '4'
+    result = WS_TREE.extract_list_elements()
+    assert len(result) == 3
+
+
+def test_sort_changelists_multi_cl_sourceset_sort_remove_empty_returns_xml():
+    test_input = InputData(
+        workspace_path=Path('file'),
+        workspace_xml=data_provider.get_multi_changelist_xml(),
+        sort_mode=SortMode.SOURCESET,
+        remove_empty=True,
+    )
+    with (pytest.MonkeyPatch().context() as c):
+        c.setattr(ElementTree, 'write', save_write)
+        sort_changelists(test_input)
+    assert TAG == 'project'
+    assert VERSION == '4'
+    result = WS_TREE.extract_list_elements()
+    assert len(result) == 2
+
