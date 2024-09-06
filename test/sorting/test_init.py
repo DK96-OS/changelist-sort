81ff0fe97ba36aff733bef2255b9284dfc6ecc86
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2024-09-06 10:36:27 -0400

    Sorting Package Tests: *u test_init - add test cases

--- test/sorting/test_init.py
+++ test/sorting/test_init.py
@@ -105,3 +105,41 @@ def test_sort_app_cl_gradle_file_returns_new_gradle_cl():
     assert len(result[0].changes) == 0
     assert len(result[1].changes) == 1
     assert result[1].changes[0].after_path == data_provider.APP_GRADLE_PATH
+
+
+def test_sort_app_cl_gradle_file_sourceset_mode_returns_new_gradle_cl():
+    test_input = [
+        ChangelistData(
+            id='1234',
+            name='App',
+            changes=[
+                data_provider.get_app_gradle_build_change_data(),
+            ],
+        )
+    ]
+    result = sort(test_input, SortMode.SOURCESET)
+    assert result != test_input
+    assert len(result) == 2
+    assert result[0].name == 'App'
+    assert result[1].name == 'Build Updates'
+    # Check Contents of Changes List
+    assert len(result[0].changes) == 0
+    assert len(result[1].changes) == 1
+    assert result[1].changes[0].after_path == data_provider.APP_GRADLE_PATH
+
+
+def test_sort_module_cl_sourceset_mode_returns_new_sourceset_cl():
+    test_input = [
+        data_provider.get_module_changelist()
+    ]
+    result = sort(test_input, SortMode.SOURCESET)
+    assert result != test_input
+    assert len(result) == 3
+    assert result[0].name == 'Module'
+    assert result[1].name == 'Module Test'
+    assert result[2].name == 'Module Main'
+    # Check Files
+    assert len(result[0].changes) == 0
+    assert len(result[1].changes) == 1
+    assert len(result[2].changes) == 1
+    
