1db6ac4c1867a47eae716c942021d3cacc2d77e0
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2024-12-04 16:15:07 -0500

    Test Sorting Package: * Update test/sorting/conftest.py - add sorting_config test fixtures * Update test/sorting/test_sorting_changelist.py - add test cases

--- test/sorting/conftest.py
+++ test/sorting/conftest.py
@@ -1,20 +1,115 @@
 import pytest
 
-from changelist_sort.list_key import ListKey
+from changelist_sort.list_key import ListKey, compute_key
 from changelist_sort.sorting import SortingChangelist
 from changelist_sort.sorting.module_type import ModuleType
 from changelist_sort.sorting.sorting_file_pattern import SortingFilePattern
 
 
 @pytest.fixture
-def shell_scripts_file_pattern():
+def shell_scripts_file_pattern() -> SortingFilePattern:
     return SortingFilePattern(file_ext='sh')
 
 
 @pytest.fixture
-def shell_scripts_changelist(shell_scripts_file_pattern):
+def shell_scripts_changelist(shell_scripts_file_pattern) -> SortingChangelist:
     return SortingChangelist(
         module_type=ModuleType.ROOT,
         list_key=ListKey(key='shellscripts', changelist_name='Shell Scripts'),
         file_patterns=[shell_scripts_file_pattern],
     )
+
+
+@pytest.fixture
+def sort_config_empty() -> SortingChangelist:
+    return SortingChangelist(
+        module_type=None,
+        list_key=compute_key(''),
+        file_patterns=[],
+    )
+
+
+@pytest.fixture
+def sort_config_all() -> SortingChangelist:
+    return SortingChangelist(
+        module_type=None,
+        list_key=compute_key('all'),
+        file_patterns=[
+            SortingFilePattern(inverse=True)
+        ],
+    )
+
+
+@pytest.fixture
+def sort_config_root_markdown_docs() -> list[SortingChangelist]:
+    return [
+        SortingChangelist(
+            module_type=ModuleType.ROOT,
+            list_key=compute_key('documentation'),
+            file_patterns=[
+                SortingFilePattern(file_ext='md'),
+            ],
+        ),
+    ]
+
+
+@pytest.fixture
+def sort_config_github_workflows_yml() -> list[SortingChangelist]:
+    return [
+        SortingChangelist(
+            module_type=None,
+            list_key=ListKey('githubworkflows', 'GitHub Workflows'),
+            file_patterns=[
+                SortingFilePattern(first_dir='.github'),
+                SortingFilePattern(path_end='workflows'),
+                SortingFilePattern(path_end='workflows/'),
+                SortingFilePattern(file_ext='yml'),
+            ],
+        ),
+    ]
+
+
+@pytest.fixture
+def sort_config_github_workflows_yml_2() -> list[SortingChangelist]:
+    return [
+        SortingChangelist(
+            module_type=ModuleType.HIDDEN,
+            list_key=compute_key('github_workflows'),
+            file_patterns=[
+                SortingFilePattern(path_start='.github/workflows/'),
+                SortingFilePattern(path_start='.github/workflows'),
+                SortingFilePattern(path_start='/.github/workflows'),
+                SortingFilePattern(file_ext='.yml'),
+            ],
+        ),
+    ]
+
+
+@pytest.fixture
+def sort_config_python_test_files() -> list[SortingChangelist]:
+    return [
+        SortingChangelist(
+            module_type=ModuleType.MODULE,
+            list_key=compute_key('Test Files'),
+            file_patterns=[
+                SortingFilePattern(filename_prefix='test'),
+                SortingFilePattern(file_ext='py'),
+            ],
+        ),
+    ]
+
+
+@pytest.fixture
+def sort_config_python_reader_files() -> list[SortingChangelist]:
+    """ Matches Python files with names ending "reader".
+    """
+    return [
+        SortingChangelist(
+            module_type=ModuleType.MODULE,
+            list_key=compute_key('Python Reader Files'),
+            file_patterns=[
+                SortingFilePattern(filename_suffix='reader'),
+                SortingFilePattern(file_ext='py'),
+            ],
+        ),
+    ]
--- test/sorting/test_sorting_changelist.py
+++ test/sorting/test_sorting_changelist.py
@@ -26,3 +26,108 @@ def test_check_file_src_dir_input_pattern_input_data_returns_true():
 def test_check_file_src_dir_input_pattern_init_file_returns_false():
     instance = get_dc_src_input()
     assert not instance.check_file(get_change_data('/changelist_sort/__init__.py'))
+
+
+def test_check_file_root_markdown_docs_with_root_readme_cd_returns_true(
+    sort_config_root_markdown_docs,
+    root_readme_change_data
+):
+    assert sort_config_root_markdown_docs[0].check_file(root_readme_change_data)
+
+
+def test_check_file_python_reader_files_with_python_reader_cd_returns_true(
+    sort_config_python_reader_files,
+    python_reader_cd
+):
+    assert sort_config_python_reader_files[0].check_file(python_reader_cd)
+
+
+def test_check_file_python_reader_files_with_module_src_cd_returns_false(
+    sort_config_python_reader_files,
+    module_src_change_data
+):
+    assert not sort_config_python_reader_files[0].check_file(module_src_change_data)
+
+
+def test_check_file_python_test_files_with_python_test_cd_returns_true(
+    sort_config_python_test_files,
+    python_test_cd
+):
+    assert sort_config_python_test_files[0].check_file(python_test_cd)
+
+
+def test_check_file_python_test_files_with_module_test_cd_returns_false(
+    sort_config_python_test_files,
+    module_test_change_data
+):
+    assert not sort_config_python_test_files[0].check_file(module_test_change_data)
+
+
+def test_check_file_gh_workflows_with_gh_workflow_cd_returns_true(
+    sort_config_github_workflows_yml,
+    github_workflows_change_data
+):
+    assert sort_config_github_workflows_yml[0].check_file(github_workflows_change_data)
+
+
+def test_check_file_gh_workflows2_with_gh_workflow_cd_returns_true(
+    sort_config_github_workflows_yml_2,
+    github_workflows_change_data
+):
+    assert sort_config_github_workflows_yml_2[0].check_file(github_workflows_change_data)
+
+
+def test_check_file_gh_workflows_with_markdown_cd_returns_false(
+    sort_config_github_workflows_yml,
+    dependabot_change_data
+):
+    assert not sort_config_github_workflows_yml[0].check_file(dependabot_change_data)
+
+
+def test_check_file_gh_workflows2_with_gh_workflow_cd_returns_false(
+    sort_config_github_workflows_yml_2,
+    dependabot_change_data
+):
+    assert not sort_config_github_workflows_yml_2[0].check_file(dependabot_change_data)
+
+
+def test_check_file_empty_config_with_python_reader_cd_returns_false(
+    sort_config_empty,
+    python_reader_cd
+):
+    assert not sort_config_empty.check_file(python_reader_cd)
+
+
+def test_check_file_empty_config_with_root_readme_cd_returns_false(
+    sort_config_empty,
+    root_readme_change_data
+):
+    assert not sort_config_empty.check_file(root_readme_change_data)
+
+
+def test_check_file_empty_config_with_gh_workflows_cd_returns_false(
+    sort_config_empty,
+    github_workflows_change_data
+):
+    assert not sort_config_empty.check_file(github_workflows_change_data)
+
+
+def test_check_file_all_config_with_python_reader_returns_true(
+    sort_config_all,
+    python_reader_cd
+):
+    assert sort_config_all.check_file(python_reader_cd)
+
+
+def test_check_file_all_config_with_root_readme_cd_returns_true(
+    sort_config_all,
+    root_readme_change_data
+):
+    assert sort_config_all.check_file(root_readme_change_data)
+
+
+def test_check_file_all_config_with_gh_workflows_cd_returns_true(
+    sort_config_all,
+    github_workflows_change_data
+):
+    assert sort_config_all.check_file(github_workflows_change_data)
