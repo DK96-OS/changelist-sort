2ff607260107a0720fed7b2b11d4244122ded992
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2024-12-03 22:11:21 -0500

    CL-Sort Input Package: * Update __init__.py - update xml reader import, improve method _load_sorting_config and handle SystemExit instead of ParseError

--- changelist_sort/input/__init__.py
+++ changelist_sort/input/__init__.py
@@ -1,7 +1,6 @@
 """The Input Package for Changelist Sort
 """
 from pathlib import Path
-from xml.etree.ElementTree import ParseError
 
 from changelist_data.storage import load_storage
 from changelist_data.storage.changelist_data_storage import ChangelistDataStorage
@@ -12,7 +11,7 @@ from changelist_sort.input.argument_parser import parse_arguments
 from changelist_sort.input.input_data import InputData
 from changelist_sort.sorting.sort_mode import SortMode
 from changelist_sort.sorting.sorting_changelist import SortingChangelist
-from changelist_sort.xml import read_xml
+from changelist_sort.xml.reader import read_xml
 
 
 def validate_input(args_list: list[str]) -> InputData:
@@ -56,15 +55,18 @@ def _determine_sort_mode(arg_data: ArgumentData) -> SortMode:
     return SortMode.MODULE
 
 
+# These are the relative paths that are checked for sorting configuration
+_SORTING_CONFIG_PATHS = ('.changelists/sort.xml', '.changelists/sorting.xml')
+
+
 def _load_sorting_config() -> list[SortingChangelist] | None:
     """ Search for the Sorting Config file and load it.
+        - If file exists, but fails to parse, then exit.
     """
-    sorting_config_paths = ('.changelists/sort.xml', '.changelists/sorting.xml')
-    for file_path_str in sorting_config_paths:
-        f = Path(file_path_str)
-        if f.exists() and f.is_file():
+    for file_path_str in _SORTING_CONFIG_PATHS:
+        if (f := Path(file_path_str)).exists() and f.is_file():
             try:
                 return read_xml(f.read_text())
-            except ParseError:
-                return None
+            except SystemExit as e:
+                exit("Sort XML file parsing failed: " + str(e))
     return None
