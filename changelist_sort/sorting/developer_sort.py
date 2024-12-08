0ae2572850b2b7615c2244f65d71d96037cb382a
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2024-12-08 15:19:36 -0500

    CL-Sort Sorting Package: * Update developer_sort.py - remove create_is_sorted_by_developer method, improve type annotations, update variable names, export filter_patterns_by_module method (and rename it to filter_by_module) to sorting_changelist module * Update sorting_changelist.py - give module_type field a default value, rearrange constructor fields (move module_type to the end with a default value of None), add filter_by_module method, improve documentation of filter_by_module

--- changelist_sort/sorting/developer_sort.py
+++ changelist_sort/sorting/developer_sort.py
@@ -1,84 +1,66 @@
-""" Sort With Developer File Patterns.
+""" Sort With Developer's SortingChangelist FilePatterns.
 """
-from typing import Iterable, Callable
+from typing import Callable
 
 from changelist_sort.change_data import ChangeData
 from changelist_sort.changelist_map import ChangelistMap
 from changelist_sort.list_key import ListKey
-from changelist_sort.sorting import file_sort, module_sort
-from changelist_sort.sorting.module_type import ModuleType
+from changelist_sort.sorting import file_sort, module_sort, sorting_changelist
 from changelist_sort.sorting.sorting_changelist import SortingChangelist
-
-
-def filter_patterns_by_module(
-    module_type: ModuleType | None,
-    cl_patterns: Iterable[SortingChangelist],
-) -> list[SortingChangelist]:
-    """ Filter the Changelists by the ModuleType their Pattern applies to.
-    """
-    return list(filter(
-        lambda dcl: dcl.module_type is None or dcl.module_type == module_type,
-        cl_patterns
-    ))
 
 
 def sort_file_by_developer(
     cl_map: ChangelistMap,
     file: ChangeData,
-    sorting_config=None,
+    sorting_config: list[SortingChangelist] | None,
 ) -> bool:
-    """ Apply the Developer FilePattern Setting to Sort a single File into the Changelist Map.
+    """ Apply the SortingChangelist FilePattern Settings to Sort a single File into the Changelist Map.
         - Filters Patterns by matching ModuleType before checking files.
         - Fallback to Module Sort
     """
-    # Filter Developer Changelist Tuple by File's ModuleType 
     if sorting_config is None:
         return module_sort.sort_file_by_module(cl_map, file)
-    filtered_dcl_patterns = filter_patterns_by_module(
+    # Filter Sorting Changelists
+    filtered_scl_patterns = sorting_changelist.filter_by_module(
         file_sort.get_module_type(file),
         sorting_config,
     )
-    # Check Developer Changelists in Tuple Order
-    for dcl_pattern in filtered_dcl_patterns:
-        if dcl_pattern.check_file(file):
-            # Pattern Matched.
+    # Check Changelists in Order
+    for scl_pattern in filtered_scl_patterns:
+        if scl_pattern.check_file(file): # Pattern Matched.
             # Search Map. Add File to Changelist.
-            if (cl := cl_map.search(dcl_pattern.list_key.key)) is not None:
+            if (cl := cl_map.search(scl_pattern.list_key.key)) is not None:
                 cl.changes.append(file)
                 return True
-            # Create the Developer Changelist. Add File to Changelist.
-            cl_map.create_changelist(dcl_pattern.list_key.changelist_name).changes.append(file)
+            # Create Changelist. Add File to Changelist
+            cl_map.create_changelist(scl_pattern.list_key.changelist_name).changes.append(file)
             return True
-    # Fallback to Module Sort when Developer Sort Fails.
+    # Fallback to Module Sort
     return module_sort.sort_file_by_module(cl_map, file)
-
-
-def create_is_sorted_by_developer(sorting_config) -> Callable[[ListKey, ChangeData], bool]:
-    return lambda x, y: is_sorted_by_developer(x, y, sorting_config)
 
 
 def is_sorted_by_developer(
     changelist_key: ListKey,
     file: ChangeData,
-    sorting_config: list[SortingChangelist],
+    sorting_config: list[SortingChangelist] | None,
 ) -> bool:
     """ Determines if this File matches the ChangeList Key or Name.
-        - Finds the First DeveloperChangelist Pattern that matches
+        - Finds the First SortingCL FilePattern match
         - Fallback to Module Sort
     """
-    # Filter Developer Changelist Tuple by File's ModuleType 
-    filtered_dcl_patterns = filter_patterns_by_module(
+    if sorting_config is None:
+        return module_sort.is_sorted_by_module(changelist_key, file)
+    # Filter Sorting Changelists
+    filtered_scl_patterns = sorting_changelist.filter_by_module(
         file_sort.get_module_type(file),
         sorting_config,
     )
-    # Check Developer Changelists in Tuple Order
-    for dcl_pattern in filtered_dcl_patterns:
-        if dcl_pattern.check_file(file):
-            # Pattern Matched
-            if dcl_pattern.list_key.key == changelist_key.key or\
-                dcl_pattern.list_key.changelist_name == changelist_key.changelist_name:
+    # Check Changelists in Order
+    for scl_pattern in filtered_scl_patterns:
+        if scl_pattern.check_file(file): # Pattern Matched
+            if scl_pattern.list_key.key == changelist_key.key or\
+                scl_pattern.list_key.changelist_name == changelist_key.changelist_name:
                 return True
-            # This File could be sorted higher in the Developer Changelist order.
-            return False
-    # Fallback to Module Sort.
+            return False # File could be higher in the Sorting order
+    # Fallback to Module Sort
     return module_sort.is_sorted_by_module(changelist_key, file)
--- changelist_sort/sorting/sorting_changelist.py
+++ changelist_sort/sorting/sorting_changelist.py
@@ -14,12 +14,12 @@ class SortingChangelist:
 
     Parameters:
     - list_key (ListKey): The Key and Name of the Changelist.
-    - module_type (ModuleType): The Type of Module to match Files with.
-    - file_patterns (tuple[DeveloperFilePattern]): 
+    - file_patterns (list[SortingFilePattern]): The FilePatterns
+    - module_type (ModuleType): The Type of Module to match Files with. None will allow matching with all modules.
     """
-    module_type: ModuleType | None
     list_key: ListKey
     file_patterns: list[SortingFilePattern]
+    module_type: ModuleType | None = None
 
     def check_file(self, file: ChangeData) -> bool:
         """ Determine if the File can be added to this Changelist.
@@ -37,3 +37,31 @@ class SortingChangelist:
             if not fp.check_file(file):
                 return False
         return True
+
+
+def filter_by_module(
+    module_type: ModuleType | None,
+    sorting_cl_list: list[SortingChangelist],
+) -> list[SortingChangelist]:
+    """ Filter Sorting Changelists by ModuleType.
+
+    This function filters a list of SortingChangelists based on the provided `module_type` argument.
+    - SortingCL with a None ModuleType always pass through the filter. This is necessary to preserve file pattern order.
+    - When None is passed as ModuleType argument, only SortingCL with None ModuleType pass through.
+
+    Parameters:
+    - module_type (ModuleType | None): The target module type to filter by. If None, only SortingChangelists with a None ModuleType will be included.
+    - sorting_cl_list (list[SortingChangelist]): The list of SortingChangelists to filter.
+
+    Returns:
+    list[SortingChangelist]: A new list containing the filtered SortingChangelists. May be empty.
+    """
+    if sorting_cl_list is None or len(sorting_cl_list) == 0:
+        return []
+    if module_type is None:
+        filter_lambda = lambda scl: scl.module_type is None
+    else:
+        filter_lambda = lambda scl: scl.module_type is None or scl.module_type == module_type
+    return list(
+        filter(filter_lambda, sorting_cl_list)
+    )
