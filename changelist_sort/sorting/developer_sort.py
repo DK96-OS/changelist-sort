d6a0457ba620e92a9988e8119e279edfa1aecb33
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2024-12-08 09:04:03 -0500

    CL-Sort Sorting Package: * Update developer_sort.py - remove SortingFilePattern definitions, remove DeveloperCLTuple definition, modify filter_patterns_by_module method (remove default DeveloperCLTuple from cl_pattern parameter)

--- changelist_sort/sorting/developer_sort.py
+++ changelist_sort/sorting/developer_sort.py
@@ -2,173 +2,17 @@
 """
 from typing import Iterable, Callable
 
-from changelist_sort import list_key
 from changelist_sort.change_data import ChangeData
 from changelist_sort.changelist_map import ChangelistMap
 from changelist_sort.list_key import ListKey
 from changelist_sort.sorting import file_sort, module_sort
 from changelist_sort.sorting.module_type import ModuleType
 from changelist_sort.sorting.sorting_changelist import SortingChangelist
-from changelist_sort.sorting.sorting_file_pattern import SortingFilePattern
-
-
-_SRC_DIR_PATTERN = SortingFilePattern(first_dir='changelist_sort')
-_TEST_DIR_PATTERN = SortingFilePattern(first_dir='test')
-_INPUT_PACKAGE_PATTERN = SortingFilePattern(path_end='input')
-_SORTING_PACKAGE_PATTERN = SortingFilePattern(path_end='sorting')
-_WORKSPACE_PACKAGE_PATTERN = SortingFilePattern(path_end='workspace')
-
-_BUILD_UPDATES_KEY = list_key.compute_key('Build Updates')
-
-# Modify these Patterns
-DEVELOPER_CL_TUPLE: tuple[SortingChangelist, ...] = (
-    SortingChangelist(
-        None,
-        list_key.compute_key('Documentation'),
-        [
-            SortingFilePattern(
-                file_ext='md',
-            ),
-        ]
-    ),
-    SortingChangelist(
-        ModuleType.MODULE,
-        list_key.compute_key('Input Package Tests'),
-        [
-            _TEST_DIR_PATTERN,
-            _INPUT_PACKAGE_PATTERN,
-        ]
-    ),
-    SortingChangelist(
-        ModuleType.MODULE,
-        list_key.compute_key('Sorting Package Tests'),
-        [
-            _TEST_DIR_PATTERN,
-            _SORTING_PACKAGE_PATTERN,
-        ]
-    ),
-    SortingChangelist(
-        ModuleType.MODULE,
-        list_key.compute_key('Workspace Package Tests'),
-        [
-            _TEST_DIR_PATTERN,
-            _WORKSPACE_PACKAGE_PATTERN,
-        ]
-    ),
-    # Tests that don't match a pattern
-    SortingChangelist(
-        ModuleType.MODULE,
-        list_key.compute_key('Tests'),
-        [
-            _TEST_DIR_PATTERN,
-        ]
-    ),
-    SortingChangelist(
-        ModuleType.MODULE,
-        list_key.compute_key('Input Package'),
-        [
-            _SRC_DIR_PATTERN,
-            _INPUT_PACKAGE_PATTERN,
-        ]
-    ),
-    SortingChangelist(
-        ModuleType.MODULE,
-        list_key.compute_key('Sorting Package'),
-        [
-            _SRC_DIR_PATTERN,
-            _SORTING_PACKAGE_PATTERN,
-        ]
-    ),
-    SortingChangelist(
-        ModuleType.MODULE,
-        list_key.compute_key('Workspace Package'),
-        [
-            _SRC_DIR_PATTERN,
-            _WORKSPACE_PACKAGE_PATTERN,
-        ]
-    ),
-    SortingChangelist(
-        ModuleType.MODULE,
-        list_key.compute_key('Main Package Source'),
-        [
-            _SRC_DIR_PATTERN,
-        ]
-    ),
-    SortingChangelist(
-        ModuleType.ROOT,
-        list_key.compute_key('Shell Scripts'),
-        [
-            SortingFilePattern(
-                file_ext='sh',
-            ),
-        ]
-    ),
-    SortingChangelist(
-        ModuleType.ROOT,
-        list_key.compute_key('Project Root'),
-        [
-            SortingFilePattern(
-                inverse=True,
-                first_dir='gradle',
-            ),
-            SortingFilePattern(
-                inverse=True,
-                file_ext='gradle',
-            ),
-            SortingFilePattern(
-                inverse=True,
-                file_ext='kts',
-            ),
-        ]
-    ),
-    SortingChangelist(
-        ModuleType.GRADLE,
-        _BUILD_UPDATES_KEY,
-        [
-            SortingFilePattern(
-                inverse=True,
-                first_dir='gradle',
-            ),
-            SortingFilePattern(
-                inverse=True,
-                first_dir='',
-            ),
-        ]
-    ),
-    SortingChangelist(
-        ModuleType.ROOT,
-        _BUILD_UPDATES_KEY,
-        [
-            SortingFilePattern(file_ext='gradle'),
-        ]
-    ),
-    SortingChangelist(
-        ModuleType.ROOT,
-        _BUILD_UPDATES_KEY,
-        [
-            SortingFilePattern(file_ext='properties'),
-        ]
-    ),
-    SortingChangelist(
-        ModuleType.GRADLE,
-        list_key.compute_key('Module Gradle Build Files'),
-        [
-            SortingFilePattern(
-                inverse=True,
-                first_dir='gradle',
-            ),
-            SortingFilePattern(
-                inverse=True,
-                first_dir='',
-            ),
-        ]
-    ),
-)
 
 
 def filter_patterns_by_module(
     module_type: ModuleType | None,
-    cl_patterns: Iterable[SortingChangelist] = DEVELOPER_CL_TUPLE
+    cl_patterns: Iterable[SortingChangelist],
 ) -> list[SortingChangelist]:
     """ Filter the Changelists by the ModuleType their Pattern applies to.
     """
