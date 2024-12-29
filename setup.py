d7d22b94e5aad715fda51c9af3e46f6d19ff5887
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2024-12-29 12:18:52 -0500

    Release 0.4.2 (#17)
    GitHub Workflows:
    * Update ci_run.yml - standardize test strategy matrix, upgrade test dependency pytest version 8.3.4
    
    Root Project Python Files:
    * Update setup.py - increase release version 0.4.2, exclude test packages from distribution, upgrade dependency changelist-data version 0.1.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="changelist-sort",
-    version="0.4.1",
+    version="0.4.2",
 	author='DK96-OS',
 	description='CLI Tool for Sorting Android Studio Changelists',
     long_description=open('README.md').read(),
@@ -16,9 +16,9 @@ setup(
         "Source Code": "https://github.com/DK96-OS/changelist-sort/"
 	},
 	license='GPLv3',
-    packages=find_packages(exclude=['test']),
+    packages=find_packages(exclude=['test', 'test.*']),
     install_requires=[
-        'changelist-data >= 0.1.4, < 0.2',
+        'changelist-data >= 0.1.5, < 0.2',
     ],
     entry_points={
         'console_scripts': [
