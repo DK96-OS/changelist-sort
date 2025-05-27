f68da87cd996440414af9dca9405b999830e2837
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2025-05-27 16:26:50 -0400

    CL-Sort Sorting Package: * Update __init__ - replace list type with Iterable in sort method, update docs, add method map_sort, add method generator_sort, add logical hidden methods * Update list_sort - add method generate_unsorted_change_data

--- changelist_sort/sorting/__init__.py
+++ changelist_sort/sorting/__init__.py
@@ -1,81 +1,150 @@
 """ Sorting Package.
 """
-from typing import Callable
+from typing import Callable, Iterable, Generator
 
 from changelist_sort.change_data import ChangeData
 from changelist_sort.changelist_data import ChangelistData
 from changelist_sort.changelist_map import ChangelistMap
 from changelist_sort.list_key import ListKey
 from changelist_sort.sorting import developer_sort, module_sort, source_set_sort
-from changelist_sort.sorting.list_sort import split_changelist
+from changelist_sort.sorting.list_sort import split_changelist, generate_unsorted_change_data
 from changelist_sort.sorting.sort_mode import SortMode
 from changelist_sort.sorting.sorting_changelist import SortingChangelist
 
 
 def sort(
-    initial_list: list[ChangelistData],
+    initial_list: Iterable[ChangelistData],
     sort_mode: SortMode,
     sorting_config: list[SortingChangelist] | None = None,
 ) -> list[ChangelistData]:
     """ Apply SortMode or Sorting Changelists to the Initial List.
-        - sorting_config Changelists will override sort_mode, if not None.
+ - sorting_config Changelists will override sort_mode, if not None.
 
-    Parameters:
-    - initial_list (list[ChangelistData]): The list of Changelists to be sorted.
-    - sort_mode (SortMode): The SortMode determining which sort rules to apply.
-    - sorting_config (list[SortingChangelist]?): An optional list of SortingChangelist that contains rules for sorting.
+**Parameters:**
+ - initial_list (list[ChangelistData]): The list of Changelists to be sorted.
+ - sort_mode (SortMode): The SortMode determining which sort rules to apply.
+ - sorting_config (list[SortingChangelist]?): An optional list of SortingChangelist that contains rules for sorting.
 
-    Returns:
-    list[ChangelistData] - The sorted Changelists.
+**Returns:**
+ list[ChangelistData] - The sorted Changelists.
     """
-    unsorted_files = []
+    if sorting_config is not None:
+        return list(map_sort(initial_list, sorting_config).generate_lists())
+    else:
+        return list(_mode_sort(initial_list, sort_mode).generate_lists())
+
+
+def map_sort(
+    initial_list: Iterable[ChangelistData],
+    sorting_config: list[SortingChangelist],
+) -> ChangelistMap:
+    """ Sort The Changelists using the SortingConfig and the ChangelistMap.
+
+**Parameters:**
+ - initial_list (Iterable[ChangelistData]): The Changelists before they are sorted.
+ - sorting_config (list[SortingChangelist]): The changelist sorting criteria.
+
+**Returns:**
+ ChangelistMap - A data container class with the sorted changelists within.
+    """
+    _sort_it_out(
+        (cl_map := ChangelistMap()),
+        initial_list,
+        is_sorted_callable=lambda key, cd: developer_sort.is_sorted_by_developer(key, cd, sorting_config),
+        sorting_callable=lambda cd: developer_sort.sort_file_by_developer(cl_map, cd, sorting_config),
+    )
+    return cl_map
+
+
+def generator_sort(
+    initial_list: Iterable[ChangelistData],
+    sorting_config: list[SortingChangelist],
+    verbose: bool = True,
+) -> ChangelistMap:
+    """ Sort The Changelists using the SortingConfig and the ChangelistMap.
+
+**Parameters:**
+ - initial_list (Iterable[ChangelistData]):
+ - sorting_config (list[SortingChangelist]): The set of criteria describing the sort objective.
+ - verbose (bool): Whether to print the total number of files sorted.
+
+**Returns:**
+ ChangelistMap -
+    """
+    total = sum(_sort_it_out_generator(
+        (cl_map := ChangelistMap()),
+        initial_list,
+        is_sorted_callable=lambda key, cd: developer_sort.is_sorted_by_developer(key, cd, sorting_config),
+        sorting_callable=lambda cd: developer_sort.sort_file_by_developer(cl_map, cd, sorting_config),
+    ))
+    if verbose:
+        print(f"Files Sorted: {total}")
+    return cl_map
+
+
+def _mode_sort(
+    initial_list: Iterable[ChangelistData],
+    sort_mode: SortMode,
+) -> ChangelistMap:
+    """ Sort Changelists using ChangelistMap and preset SortMode configurations.
+    """
     cl_map = ChangelistMap()
+    if sort_mode == SortMode.MODULE:
+        _sort_it_out(
+            cl_map,
+            initial_list,
+            module_sort.is_sorted_by_module,
+            lambda cd: module_sort.sort_file_by_module(cl_map, cd)
+        )
+    elif sort_mode == SortMode.SOURCESET:
+        _sort_it_out(
+            cl_map,
+            initial_list,
+            source_set_sort.is_sorted_by_source_set,
+            lambda cd: source_set_sort.sort_by_source_set(cl_map, cd)
+        )
+    else:
+        exit("SortMode not Implemented")
+    return cl_map
+
+
+def _sort_it_out(
+    cl_map: ChangelistMap,
+    initial_list: Iterable[ChangelistData],
+    is_sorted_callable: Callable[[ListKey, ChangeData], bool],
+    sorting_callable: Callable[[ChangeData], bool]
+):
+    """
+ - This method is called by higher level methods
+    """
+    unsorted = []
     for cl in initial_list:
         if not cl_map.insert(cl):
             _handle_map_insertion_error(cl_map, cl)
-        # Split CL based on SortMode criteria
-        unsorted_files.extend(
-            split_changelist(cl, _get_is_sorted_callable(sort_mode, sorting_config))
+        # Extract Unsorted ChangeData using IsSortedCallable
+        unsorted.extend(
+            list_sort.generate_unsorted_change_data(cl, is_sorted_callable)
         )
-    # This Callable depends on SortMode.
-    #  It determines map keys, and executes map insertions.
-    sorting_callable = _create_sorting_callable(cl_map, sort_mode, sorting_config)
-    for cd in unsorted_files:
+    # Execute SortingCallable on each Unsorted File
+    for cd in unsorted:
         sorting_callable(cd)
-    return cl_map.get_lists()
 
 
-def _create_sorting_callable(
-    changelist_map: ChangelistMap,
-    sort_mode: SortMode,
-    sorting_config: list[SortingChangelist] | None,
-) -> Callable[[ChangeData], bool]:
-    """ Create a Callable that sorts ChangeData passed to it.
+def _sort_it_out_generator(
+    cl_map: ChangelistMap,
+    initial_list: Iterable[ChangelistData],
+    is_sorted_callable: Callable[[ListKey, ChangeData], bool],
+    sorting_callable: Callable[[ChangeData], bool]
+) -> Generator[bool, None, None]:
     """
-    if sorting_config is not None:
-        return lambda cd: developer_sort.sort_file_by_developer(changelist_map, cd, sorting_config)
-    if sort_mode == SortMode.MODULE:
-        return lambda cd: module_sort.sort_file_by_module(changelist_map, cd)
-    if sort_mode == SortMode.SOURCESET:
-        return lambda cd: source_set_sort.sort_by_source_set(changelist_map, cd)
-    else:
-        exit("SortMode not Implemented")
-
-
-def _get_is_sorted_callable(
-    sort_mode: SortMode,
-    sorting_config: list[SortingChangelist] | None,
-) -> Callable[[ListKey, ChangeData], bool]:
-    """ Obtain a Callable that determines whether a ChangeData is sorted.
+ - This method may be called by higher level methods.
     """
-    if sorting_config is not None:
-        return lambda key, cd: developer_sort.is_sorted_by_developer(key, cd, sorting_config)
-    if sort_mode == SortMode.MODULE:
-        return module_sort.is_sorted_by_module
-    if sort_mode == SortMode.SOURCESET:
-        return source_set_sort.is_sorted_by_source_set
-    else:
-        exit("SortMode not Implemented")
+    for cl in initial_list:
+        if not cl_map.insert(cl):
+            _handle_map_insertion_error(cl_map, cl)
+    for cl in initial_list: # Extract Unsorted ChangeData using IsSortedCallable
+        for cd in list_sort.generate_unsorted_change_data(cl, is_sorted_callable):
+            yield sorting_callable(cd) # Execute SortingCallable on each Unsorted File
 
 
 def _handle_map_insertion_error(
--- changelist_sort/sorting/list_sort.py
+++ changelist_sort/sorting/list_sort.py
@@ -1,6 +1,6 @@
 """ Changelist Sorting Methods.
 """
-from typing import Callable
+from typing import Callable, Generator
 
 from changelist_sort.change_data import ChangeData
 from changelist_sort.changelist_data import ChangelistData
@@ -11,24 +11,36 @@ def split_changelist(
     changelist: ChangelistData,
     is_sorted: Callable[[ListKey, ChangeData], bool],
 ) -> list[ChangeData]:
-    """
-    Split the Changelist by checking that all changes are sorted.
-        Removes each element that is returned from the changelist.
+    """ Split the Changelist by checking that all changes are sorted.
+ - Removes each element that is returned from the changelist.
 
-    Parameters:
-    - changelist (ChangelistData): The Changelist to split based on sorting function.
-    - is_sorted (callable[bool]): A Function that determines whether a Change is Sorted.
+**Parameters:**
+ - changelist (ChangelistData): The Changelist to split based on sorting function.
+ - is_sorted (callable[bool]): A Function that determines whether a Change is Sorted.
 
-    Returns:
-    list[ChangeData] - The List of ChangeData that are Unsorted, now removed from this changelist.
+**Returns:**
+ list[ChangeData] - The List of ChangeData that are Unsorted, now removed from this changelist.
     """
-    unsorted_files = []
+    return list(generate_unsorted_change_data(changelist, is_sorted))
+
+
+def generate_unsorted_change_data(
+    changelist: ChangelistData,
+    is_sorted: Callable[[ListKey, ChangeData], bool],
+) -> Generator[ChangeData, None, None]:
+    """ Iterates through the ChangeData files, popping and yielding those that are unsorted.
+- Modifies the ChangelistData by popping any valid number of items from the changes list.
+
+**Parameters:**
+ - changelist (ChangelistData): The Changelist containing File Change data to check if sorted.
+ - is_sorted (Callable): The callable function that determines whether a FileChange is sorted in this Changelist.
+
+**Yields:**
+ Generator[ChangeData] - The ChangeData objects that are not sorted in this Changelist, and have been popped from the list.
+    """
     for index in range(len(changelist.changes) - 1, -1, -1):
         if not is_sorted(
             changelist.list_key,
             changelist.changes[index]
         ):
-            unsorted_files.append(
-                changelist.changes.pop(index)
-            )
-    return unsorted_files
+            yield changelist.changes.pop(index)
