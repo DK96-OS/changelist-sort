5fd185884094ac8a3b7f2268170ac8c09db8a9fb
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2025-05-28 14:46:16 -0400

    Test Sorting Package: * Update test_method_generator_sort - add test cases, update return type handling (wrap in list)

--- test/changelist_sort/sorting/test_method_generator_sort.py
+++ test/changelist_sort/sorting/test_method_generator_sort.py
@@ -9,24 +9,36 @@ from changelist_sort.sorting import generator_sort
 def test_generator_sort_empty_input_empty_config_returns_empty_cl_map():
     test_input = []
     test_config = []
-    result = generator_sort(test_input, test_config)
-    assert len(result.get_lists()) == 0
+    result = list(generator_sort(test_input, test_config))
+    assert len(result) == 0
 
 
 def test_generator_sort_empty_input_empty_config_verbose_prints_zero_returns_empty():
     test_input = []
     test_config = []
-    result = generator_sort(test_input, test_config)
-    assert len(result.get_lists()) == 0
+    result = list(generator_sort(test_input, test_config))
+    assert len(result) == 0
 
 
 def test_generator_sort_simple_storage_input_empty_config_returns_cl_map(simple_storage: ChangelistDataStorage):
     test_input = expand_changelists(simple_storage.get_changelists())
-    result = generator_sort(test_input, [])
-    assert len(list(result.generate_nonempty_lists())) == 1
+    result = list(generator_sort(test_input, [], True))
+    assert len(list(result)) == 1
 
 
-def test_generator_sort_multi_storage_input_empty_config_returns_cl_map(multi_storage: ChangelistDataStorage):
+def test_generator_sort_simple_storage_input_empty_config_no_filter_returns_2(simple_storage: ChangelistDataStorage):
+    test_input = expand_changelists(simple_storage.get_changelists())
+    result = list(generator_sort(test_input, [], False))
+    assert len(list(result)) == 2
+
+
+def test_generator_sort_multi_storage_input_empty_config_filter_returns_2(multi_storage: ChangelistDataStorage):
     test_input = expand_changelists(multi_storage.get_changelists())
-    result = generator_sort(test_input, [])
-    assert len(list(result.generate_nonempty_lists())) == 2
+    result = list(generator_sort(test_input, [], True))
+    assert len(list(result)) == 2
+
+
+def test_generator_sort_multi_storage_input_empty_config_no_filter_returns_3(multi_storage: ChangelistDataStorage):
+    test_input = expand_changelists(multi_storage.get_changelists())
+    result = list(generator_sort(test_input, [], False))
+    assert len(list(result)) == 3
