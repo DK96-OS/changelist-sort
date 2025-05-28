b2b2018f64596c8eaf410f2d080f30dbe4e3aff0
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2025-05-27 21:04:24 -0400

    Test Input Package: * Update test_method_validate_input - debug test cases, add test cases

--- test/input/test_method_validate_input.py
+++ test/input/test_method_validate_input.py
@@ -1,28 +1,22 @@
 """ Testing Input Package Init Module Methods.
 """
+from io import StringIO
 from pathlib import Path
 from unittest.mock import Mock
 
 import pytest
 
-from changelist_data.storage import StorageType, storage_type
-
 from changelist_sort.input import validate_input
 from changelist_sort.sorting.sort_mode import SortMode
+from test.xml.test_generator import INITIAL_ELEMENT_TREE
 
 
-def test_validate_input_no_args_ws_file_does_not_exist_raises_exit(monkeypatch):
-    test_input = []
+def test_validate_input_no_args_no_files_exist_returns_basic_input_dat(monkeypatch):
     monkeypatch.setattr(Path, 'exists', lambda _: False)
-    #
-    result = validate_input(test_input)
-    #
-    assert len(result.storage.get_changelists()) == 0
-    assert result.storage.storage_type == StorageType.CHANGELISTS
-    assert result.storage.update_path == storage_type.get_default_path(StorageType.CHANGELISTS)
-    #
-    assert result.sort_mode == SortMode.MODULE
+    result = validate_input([])
     assert not result.remove_empty
+    assert len(result.sorting_config) == 0
+    assert not result.generate_sort_xml
 
 
 def test_validate_input_no_args_ws_file_is_empty_raises_exit(monkeypatch):
@@ -41,7 +35,7 @@ def test_validate_input_no_args_ws_file_has_no_cl_(no_changelist_xml):
         c.setattr(Path, 'is_file', lambda _: True)
         c.setattr(Path, 'read_text', lambda _: no_changelist_xml)
         result = validate_input(test_input)
-    assert len(result.storage.get_changelists()) == 0
+        assert len(result.storage.get_changelists()) == 0
 
 
 def test_validate_input_no_args_ws_file_simple_cl_(get_cl_simple_xml):
@@ -54,7 +48,7 @@ def test_validate_input_no_args_ws_file_simple_cl_(get_cl_simple_xml):
         c.setattr(Path, 'stat', lambda _: obj)
         c.setattr(Path, 'read_text', lambda _: get_cl_simple_xml)
         result = validate_input(test_input)
-    assert len(result.storage.get_changelists()) == 1
+        assert len(result.storage.get_changelists()) == 1
 
 
 def test_validate_input_no_args_ws_file_multi_cl_(multi_storage, get_cl_multi_xml):
@@ -133,6 +127,45 @@ def test_validate_input_remove_empty(no_changelist_xml):
         assert result.sort_mode == SortMode.MODULE
         assert result.remove_empty
         assert len(result.storage.get_changelists()) == 0
+
+
+def test_validate_input_data_file_changelist(monkeypatch, simple_changelist_xml):
+    test_input = ['--data_file', '.changelists/data.xml']
+    monkeypatch.setattr(Path, 'exists', lambda x: x.name == 'data.xml')
+    monkeypatch.setattr(Path, 'read_text', lambda _: simple_changelist_xml)
+    result = validate_input(test_input)
+    assert result.storage.update_path == Path(test_input[1])
+    assert len(result.sorting_config) == 0
+
+
+def test_validate_input_sort_xml_file_argument_does_not_exist_raises_exit(monkeypatch):
+    test_input = ['--sort_xml_file', '.changelists/sort.xml']
+    monkeypatch.setattr(Path, 'exists', lambda _: False)
+    with pytest.raises(SystemExit, match='Sort XML file'):
+        result = validate_input(test_input)
+
+
+def test_validate_input_sort_xml_file_is_empty_returns_empty_sorting_config(monkeypatch):
+    test_input = ['--sort_xml_file', '.changelists/sort.xml']
+    monkeypatch.setattr(Path, 'exists', lambda x: x.name == 'sort.xml')
+    monkeypatch.setattr(Path, 'read_text', lambda _: '')
+    result = validate_input(test_input)
+    assert not result.generate_sort_xml
+
+
+def test_validate_input_sort_xml_file_initial_element_tree_returns_sorting_config(monkeypatch):
+    test_input = ['--sort_xml_file', '.changelists/sort.xml']
+    monkeypatch.setattr(Path, 'exists', lambda x: x.name == 'sort.xml')
+    INITIAL_ELEMENT_TREE.write(
+        (buffer := StringIO()),
+        encoding='unicode',
+        xml_declaration=False,
+    )
+    monkeypatch.setattr(Path, 'read_text', lambda _: buffer.getvalue())
+    #
+    result = validate_input(test_input)
+    assert not result.generate_sort_xml
+    assert len(result.sorting_config) == 3
 
 
 def test_validate_input_generate_sort_xml_():
