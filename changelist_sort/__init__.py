9a9451536c1caae691df8c81d1c0d6e9c24690a2
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2025-05-28 17:59:52 -0400

    CL-Sort Main Package: * Update __init__ - update docs, remove unreleased method sort_and_filter (the same method exists in sorting package)

--- changelist_sort/__init__.py
+++ changelist_sort/__init__.py
@@ -1,19 +1,22 @@
 """ Main Package Methods.
 """
-from typing import Iterable, Generator
+from typing import Iterable
 
 from changelist_data import ChangelistDataStorage
 from changelist_data.changelist import Changelist
 
 from changelist_sort.changelist_data import ChangelistData, generate_simple_changelists, generate_expanded_changelists
 from changelist_sort.input.input_data import InputData
-from changelist_sort.sorting import sort, SortMode, SortingChangelist, map_sort
+from changelist_sort.sorting import sort, SortMode, SortingChangelist
 
 
 def sort_changelists(
     input_data: InputData,
 ):
     """ Sort the given Changelists and write them to the Workspace File.
+
+**Parameters:**
+ - input_data (InputData): The program input, containing data and options.
     """
     sort_changelist_in_storage(
         input_data.storage,
@@ -33,6 +36,12 @@ def sort_changelist_in_storage(
     sorting_config: list[SortingChangelist],
 ):
     """ Sort the Changelists in Storage, and update them.
+
+**Parameters:**
+ - storage (ChangelistDataStorage): The Storage object with read and write access to changelists data.
+ - sort_mode (SortMode): The Sorting Mode to apply during operation.
+ - remove_empty (bool): Whether to remove empty Changelists, before writing to storage.
+ - sorting_config (list[SortingChangelist]): The configuration for sorting criteria.
     """
     changelists = sort(
         initial_list=generate_expanded_changelists(storage.generate_changelists()),
@@ -46,37 +55,15 @@ def sort_changelist_in_storage(
     )
 
 
-def sort_and_filter(
-    data: Iterable[ChangelistData],
-    sorting_config: list[SortingChangelist],
-    sort_mode: SortMode = SortMode.MODULE,
-    filter_empty: bool = True,
-) -> Generator[ChangelistData, None, None]:
-    """
-**Parameters:**
- - data (Iterable[ChangelistData]): The input data.
- - sort_mode (SortMode): The mode to apply in absence of sorting_config.
- - sorting_config (list[SortingChangelist]): The Sorting Changelists configuration.
- - apply_filter (bool): Filter out Empty Changelists. Default: True.
-
-**Yields:**
- ChangelistData - The sorted ChangelistData objects.
-    """
-    cl_map = map_sort(data, sorting_config)
-    #
-    if filter_empty:
-        yield from cl_map.generate_nonempty_lists()
-    else:
-        yield from cl_map.generate_lists()
-
-
 def simplify_changelists(
     data: Iterable[ChangelistData]
 ) -> list[Changelist]:
+    """ Convert to ChangelistData Storage DataClass."""
     return list(generate_simple_changelists(data))
 
 
 def expand_changelists(
     data: Iterable[Changelist]
 ) -> list[ChangelistData]:
+    """ Convert from ChangelistData Storage DataClass to Sort DataClass."""
     return list(generate_expanded_changelists(data))
